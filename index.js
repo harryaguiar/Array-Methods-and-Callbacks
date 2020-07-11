8f3d4a244d21ce7413d6d613f937244f252882ed
Harrison Aguiar <harryaguiar@gmail.com>
2020-07-10 20:21:27 -0400

    task 2 done

--- index.js
+++ index.js
@@ -12,14 +12,26 @@ console.log('its working');
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
 
+// console.log(fifaData[Year]);
+
+// const filterWorldCup2014 = fifaData.filter( function(data){
+//     return data.Year > 2014
+// });
+
+// console.log(filterWorldCup2014);
+
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+const finalsData = fifaData.filter(
 
-    /* code here */
+function getFinals(data) {
+    if (data["Stage"] === "Final") {
+        return true
+    }
+});
 
-};
+console.log(finalsData);
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
